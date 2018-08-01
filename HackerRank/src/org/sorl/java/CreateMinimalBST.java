632a6074e2ceceba1744f200e8af2f7fc17de7db
srinivas <srinivas@WIN10TEST1.AHRCNYC.ORG>
2018-08-01 11:44:05 -0400

    first change.

--- HackerRank/src/org/sorl/java/CreateMinimalBST.java
+++ HackerRank/src/org/sorl/java/CreateMinimalBST.java
@@ -117,13 +117,14 @@ public class CreateMinimalBST {
 
 	private static void createBinary() {
 		// TODO Auto-generated method stub
-		Node first = new Node(1);
+	/*	Node first = new Node(1);
 		Node second = new Node(4);
 		Node third = new Node(3);
 		Node fourth = new Node(2);
 		Node fifth = new Node(5);
 		Node sixth = new Node(6);
 		Node seventh = new Node(7);
+		*/
 
 	}
 }
